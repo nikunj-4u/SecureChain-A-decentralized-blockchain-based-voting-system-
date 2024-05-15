a4fc76bd6280069adcff81adc9a62ea885b96213
Nikunj <aroranikunj277@gmail.com>
2024-05-15 16:23:12 +0530

    build.sh updated

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Step 1: Run node script.js
+# Step 1: Install uvicorn
+pip install uvicorn
+
+# Step 2: Install truffle
+npm install -g truffle
+
+# Step 3: Run node script.js
 node script.js &
 
-# Step 2: Start the database server
+# Step 4: Start the database server
 cd Database_API
 uvicorn main:app --reload --host 0.0.0.0 &
 
-# Step 3: Compile smart contracts with Truffle
+# Step 5: Compile smart contracts with Truffle
 cd ..
 truffle console --eval "compile" &
-
-# Step 4: Migrate the Truffle contract to the local blockchain
-gnome-terminal -- truffle migrate
